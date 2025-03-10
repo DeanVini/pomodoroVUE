9b4cc2e709817ececccbe3c730b37e35b88c71a9
DeanVini <deanvinicius2003@gmail.com>
2025-03-10 01:23:11 -0300

    fix: verce

--- server.cjs
+++ server.cjs
@@ -1,29 +1,10 @@
-const express = require("express");
-const jsonServer = require("json-server");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const path = require("path");
+import jsonServer from "json-server";
 
-const app = express();
-const apiServer = jsonServer.create();
+const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
 
-app.use(express.static("dist")); // Serve o frontend
+server.use(middlewares);
+server.use(router);
 
-app.use(
-  "/api",
-  createProxyMiddleware({
-    target: "http://localhost:3001",
-    changeOrigin: true,
-  })
-);
-
-apiServer.use(middlewares);
-apiServer.use(router);
-apiServer.listen(3001, () => {
-  console.log("JSON Server rodando na porta 3001");
-});
-
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
-});
+export default server;
