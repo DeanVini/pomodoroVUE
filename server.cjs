e59334b4636b63082b5e425d9792188f8cc9b649
DeanVini <deanvinicius2003@gmail.com>
2025-08-11 20:44:42 -0300

    fix: update server configuration and routes in vercel.json

--- server.cjs
+++ server.cjs
@@ -1,29 +1,18 @@
 const express = require("express");
 const jsonServer = require("json-server");
-const { createProxyMiddleware } = require("http-proxy-middleware");
 const path = require("path");
 
 const app = express();
-const apiServer = jsonServer.create();
 const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
 
-app.use(express.static("dist")); // Serve o frontend
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.use(
-  "/api",
-  createProxyMiddleware({
-    target: "http://localhost:3001",
-    changeOrigin: true,
-  })
-);
+app.use("/api", middlewares, router);
 
-apiServer.use(middlewares);
-apiServer.use(router);
-apiServer.listen(3001, () => {
-  console.log("JSON Server rodando na porta 3001");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
-});
+module.exports = app;
+
